c007ecbaf9107ca0974c91311442285423d0ddbd
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2021-08-07 06:44:53 +0100

    footer layout

---
+++ resources/views/layouts/footer.php
@@ -0,0 +1,5 @@
+<footer class="page-footer font-small blue">
+<!-- Copyright -->
+<div align="center" style="color: blue"><a href="http://twitter.com/@sparklegrid"> © 2021 All right reserved Unification FC  | Sparklegrid </a>
+</div>
+</footer>

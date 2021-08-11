c7906dcaf828b9b21f235f75f1facb6c2297c563
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2021-08-11 23:36:14 +0100

    navbar, donation & contact link update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,7 +72,13 @@
                         <a class="nav-link" href="{{ url('/') }}" style="color: white">Welcome</a>
       </li>
       
+      <li class="nav-link">     
+                        <a class="nav-link" href="{{ url('/') }}" style="color: white">Shop for Sports kit</a>
+      </li>
 
+      <li class="nav-link">     
+                        <a class="nav-link" href="{{ url('/') }}" style="color: white">Donation</a>
+      </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search">
@@ -93,7 +99,7 @@
                         <div class="p-6">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Welcome</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Welcome</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -138,7 +144,7 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">Events & Gallery</div>
+                        <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Events & Gallery</a></div>
                             </div>
 
                             <div class="ml-12">
@@ -168,16 +174,24 @@
                             <a href="#" class="ml-1 underline">
                                 Contact us / Send us feedback
                             </a>
+
+                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" viewBox="0 0 26 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
+                                <path d="M5.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
+                            </svg>
+
+                            <a href="#" class="ml-1 underline">
+                                Donation 
+                            </a>
                         </div>
                     </div>
 
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+                    <div class="ml-4 text-center text-sm text-black-500 sm:text-right sm:ml-0">
                         WE ARE #TheIndomitables #teamunification
                     </div>
                 </div>
             </div>
         </div>
-        <br><br>
+        <br>
         @include ('layouts.footer') 
     </body>
 </html>

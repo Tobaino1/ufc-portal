9f263f00233700cfadb0b4a4a8d502c2b34abd50
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2021-09-01 20:38:35 +0100

    players table

---
+++ database/migrations/2021_08_31_143512_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('position');
+            $table->string('role'); //captain, asst. captain, stand in captain
+            $table->string('dominantfoot');
+            $table->string('image');
+            $table->text('detail'); //no of caps, goals, assts
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}

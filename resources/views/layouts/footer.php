47727d02d636f275b73e8210f66db5a1b102360c
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2021-09-01 20:32:42 +0100

    addition of trdemark

--- resources/views/layouts/footer.php
+++ resources/views/layouts/footer.php
@@ -1,5 +1,5 @@
 <footer class="page-footer font-small blue">
 <!-- Copyright -->
-<div align="center" style="color: blue"><a href="http://twitter.com/@sparklegrid"> © 2021 All right reserved Unification FC  | Sparklegrid </a>
+<div align="center" style="color: blue"><a href="http://twitter.com/@sparklegrid"> © 2020 All right reserved Unification FC  | Alliance Path </a>
 </div>
 </footer>

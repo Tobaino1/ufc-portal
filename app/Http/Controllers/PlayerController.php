3a90830720e98ea06f455a5c6a727d019e386866
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2021-09-01 20:37:56 +0100

    player controller

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+
+class PlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $players = Player::latest()->paginate(5);
+    
+        return view('players.index',compact('players'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    public function requestPlayers()
+      { 
+        $players = Player::all();
+        return view ('players_request', ['players' => $players]); 
+      }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('players.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'position' => 'required',
+            'role' => 'required',
+            'dominantfoot' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'detail' => 'required',
+        ]);
+  
+        $input = $request->all();
+  
+        if ($image = $request->file('image')) {
+            $destinationPath = 'image/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }
+    
+        Player::create($input);
+     
+        return redirect()->route('players.index')
+                        ->with('success','Player Profile created successfully.');
+    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Player $player)
+    {
+        return view('players.show',compact('player'));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Player $player)
+    {
+        return view('players.edit',compact('player'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Player $player)
+    {
+        $request->validate([
+            'name' => 'required',
+            'position' => 'required',
+            'role' => 'required',
+            'dominantfoot' => 'required',
+            'detail' => 'required',
+
+        ]);
+  
+        $input = $request->all();
+  
+        if ($image = $request->file('image')) {
+            $destinationPath = 'image/';
+            $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
+            $image->move($destinationPath, $profileImage);
+            $input['image'] = "$profileImage";
+        }else{
+            unset($input['image']);
+        }
+          
+        $player->update($input);
+    
+        return redirect()->route('players.index')
+                        ->with('success','Profile of the Player have been updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Player  $player
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Player $player)
+    {
+        $player->delete();
+     
+        return redirect()->route('players.index')
+                        ->with('success','Profile of the Player was deleted successfully');
+    }
+}

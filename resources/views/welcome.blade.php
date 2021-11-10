0147e63b7c5523d2c98db2e49747df1ddfac8b68
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2021-11-10 19:35:01 +0100

    welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,102 +1,22 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon"  href="storage/UFC LOGO.jpg">
+@extends('layouts.landing')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'UNIFICATION FOOTBALL') }}</title>
-
-<!-- Fonts -->
-<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-<!-- Styles -->
-<style>
-    /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}}
-</style>
-
-<style>
-    body {
-        font-family: 'Nunito', sans-serif;
-    }
-</style>
-
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-   <script src="{{ asset('js/aos.js') }}" ></script>
-   <script src="{{ asset('js/bootstrap.min.js') }}" ></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet"> -->
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/open-iconic-bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/ionicons.min.css') }}"> 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}"> 
-     <!-- blade template css -->
-     <link href="{{ asset('css/app.css') }}" rel="stylesheet"> 
-
-</head>
-    <body class="antialiased">
-        <!-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif -->
-            <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-
-  <a class="navbar-brand" href="#">Unification Football Club </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor01">
-    <ul class="navbar-nav mr-auto">
-      
-    <li class="nav-link">     
-                        <a class="nav-link" href="{{ url('/') }}" style="color: white">Welcome</a>
-      </li>
-      
-      <li class="nav-link">     
-                        <a class="nav-link" href="{{ url('/') }}" style="color: white">Shop for Sports kit</a>
-      </li>
-
-      <li class="nav-link">     
-                        <a class="nav-link" href="{{ url('/') }}" style="color: white">Donation</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-danger my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-        </div>
-    </nav>
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                <img src="{{ 'storage/UFC LOGO.jpg'}}" height="130" width="130" /> 
-                   
-                </div>
-
-                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+          @section('content')
+  <div class="max-w-6xl mx-auto sm:px-8 lg:px-12">
+               
+                <div class="mt-10 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                    <div class="grid grid-cols-1 md:grid-cols-6">
+                        <!-- <div class="p-2"> -->
+                            <div align="center">
+                                <img src="{{ 'storage/UFC LOGO.jpg'}}" height="130" width="130" />  
+                                <h4 style="color: blue"> UNIFICATION FOOTBALL CLUB </h4>   
+                            <!-- </div> -->
+                            </div>
+                </div> </div>
+                                                                                                                                    
+            <BR>
+                <div class="mt-10 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
-                        <div class="p-6">
+                        <div class="p-6"> 
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">Welcome</a></div>
@@ -104,11 +24,12 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                            You are welcome to the official portal of Unifiction Football Club, Nigeria League side based in Lagos with the 
+                            You are welcome to the official portal of Unification Football Club, Football club based in Lagos with the 
                             core objectives of competing in the top tier leagues, accredited tournaments & 
                             cup competitions within and outside the shores of the country, nurturing prospects and producing top notch players that
                             can compete within and outside the shores of Nigeria, exercising corporate social responsibility for purposeful impact
                             in our contemporary society. 
+                            
                                 </div>
                             </div>
                         </div>
@@ -116,12 +37,27 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="#" class="underline text-gray-900 dark:text-white">The Team</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="{{ ('team') }}" class="underline text-gray-900 dark:text-white">The Team</a></div>
                             </div>
-                                <a class="nav-link" href="#">The Players</a>
-                               <a class="nav-link" href="#">The Management</a> 
-                                <a class="nav-link" href="#">The Fans Corner</a>
+                          
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                            The Team view section gives insight about the players profile, management personnel and the fans corner.
+                             Click on the <a href="{{ ('team') }}" class="underline text-gray-900 dark:text-white">The Team</a> to for more details</a>
+                            <strong>Founded:</strong> Nov, 2019 <br>
+                            <strong>Ground: </strong>Yidi Field, Agege - Lagos, Nigeria <br>
+                            <strong>Chairman: </strong> Oyekan Oluwatobi <br>
+                            <strong>Team Manager: </strong>Animashaun Damilola<br>
+                            <!-- <strong>Technical Advisor:</strong> - <br> -->
+                            <strong>Coach: </strong> Haliru Idris <br>
+                            <strong>Asst Coach: </strong> Adeyemi Segun <br>
+                            <strong>League / Affilation: </strong> Nigeria Women Football League (NWFL-Nationwide), 
+                            Lagos State Football Association (Ekofootball), Ikeja Divisional football Association (IKEJDFA)  
 
+
+
+                            
+</div></div>
                           
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
@@ -136,7 +72,7 @@
                             </div>
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Unification Football Club News room is an information driven portal and newsletter aggregating all of the latest and most important news related to the club's activities.
+                                Unification FC News room is an information driven portal and newsletter aggregating all of the latest and most important news related to the club's activities.
                                 </div>
                             </div>
                         </div>
@@ -149,10 +85,11 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    This is the club's media library that shares some of the past events, matches, action moments, interviews, trials and scouting session, camping activities amongst others 
+                                    Club's media library that shares some of the past events, matches, action moments, interviews, trials and scouting session, camping activities amongst others 
                                 </div>
                             </div>
                         </div>
+                        
                     </div>
                 </div>
 
@@ -193,5 +130,4 @@
         </div>
         <br>
         @include ('layouts.footer') 
-    </body>
-</html>
+        @endsection

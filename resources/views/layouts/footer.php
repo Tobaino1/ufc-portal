289620fa0a4d0824afae89e7fc7edd34b62796a1
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2022-06-24 16:59:51 +0100

    year update

--- resources/views/layouts/footer.php
+++ resources/views/layouts/footer.php
@@ -1,5 +1,5 @@
 <footer class="page-footer font-small blue">
 <!-- Copyright -->
-<div align="center" style="color: blue"><a href="http://twitter.com/alliancepath"> © 2019 - 2021 All right reserved Unification FC  | Alliance Path </a>
+<div align="center" style="color: blue"><a href="http://twitter.com/alliancepath"> © 2019 - 2022 All right reserved Unification FC  | Alliance Path </a>
 </div>
 </footer>

7cf5790b97721ebea78d50a9f386c0cb4a533ad0
Oyekan Oluwatobi <39465655+Tobaino1@users.noreply.github.com>
2025-06-28 23:30:46 +0100

    create function for EMRS

---
+++ insert.php
@@ -0,0 +1,177 @@
+<?php 
+//Databse Connection file
+include('dbconnection.php');
+if(isset($_POST['submit']))
+  {
+  	//getting the post values
+    $fname=$_POST['fname'];
+    $gender=$_POST['gender'];
+    $contno=$_POST['contactno'];
+    $age=$_POST['age'];
+    $record=$_POST['record'];
+   $ppic=$_FILES["profilepic"]["name"];
+// get the image extension
+$extension = substr($ppic,strlen($ppic)-4,strlen($ppic));
+// allowed extensions
+$allowed_extensions = array(".jpg","jpeg",".png",".gif");
+// Validation for allowed extensions .in_array() function searches an array for a specific value.
+if(!in_array($extension,$allowed_extensions))
+{
+echo "<script>alert('Invalid format. Only jpg / jpeg/ png /gif format allowed');</script>";
+}
+else
+{
+//rename the image file
+$imgnewfile=md5($imgfile).time().$extension;
+// Code for move image into directory
+move_uploaded_file($_FILES["profilepic"]["tmp_name"],"profilepics/".$imgnewfile);
+// Query for data insertion
+$query=mysqli_query($con, "insert into tblusers(FirstName,gender, MobileNumber, age, record,ProfilePic) value('$fname','$gender', '$age', '$email', '$record','$imgnewfile' )");
+if ($query) {
+echo "<script>alert('You have successfully inserted the data');</script>";
+echo "<script type='text/javascript'> document.location ='home.php'; </script>";
+} else{
+echo "<script>alert('Something Went Wrong. Please try again');</script>";
+}}
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
+<title>UFC EMRS</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<link rel="shortcut icon" href="images/UFCLOGO.JPG" type="image/png">
+
+<style>
+body {
+	color: #fff;
+	background: #63738a;
+	font-family: 'Roboto', sans-serif;
+}
+.form-control {
+	height: 40px;
+	box-shadow: none;
+	color: #969fa4;
+}
+.form-control:focus {
+	border-color: #5cb85c;
+}
+.form-control, .btn {        
+	border-radius: 3px;
+}
+.signup-form {
+	width: 450px;
+	margin: 0 auto;
+	padding: 30px 0;
+  	font-size: 15px;
+}
+.signup-form h2 {
+	color: #636363;
+	margin: 0 0 15px;
+	position: relative;
+	text-align: center;
+}
+.signup-form h2:before, .signup-form h2:after {
+	content: "";
+	height: 2px;
+	width: 30%;
+	background: #d4d4d4;
+	position: absolute;
+	top: 50%;
+	z-index: 2;
+}	
+.signup-form h2:before {
+	left: 0;
+}
+.signup-form h2:after {
+	right: 0;
+}
+.signup-form .hint-text {
+	color: #999;
+	margin-bottom: 30px;
+	text-align: center;
+}
+.signup-form form {
+	color: #999;
+	border-radius: 3px;
+	margin-bottom: 15px;
+	background: #f2f3f7;
+	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+	padding: 30px;
+}
+.signup-form .form-group {
+	margin-bottom: 20px;
+}
+.signup-form input[type="checkbox"] {
+	margin-top: 3px;
+}
+.signup-form .btn {        
+	font-size: 16px;
+	font-weight: bold;		
+	min-width: 140px;
+	outline: none !important;
+}
+.signup-form .row div:first-child {
+	padding-right: 10px;
+}
+.signup-form .row div:last-child {
+	padding-left: 10px;
+}    	
+.signup-form a {
+	color: #fff;
+	text-decoration: underline;
+}
+.signup-form a:hover {
+	text-decoration: none;
+}
+.signup-form form a {
+	color: #5cb85c;
+	text-decoration: none;
+}	
+.signup-form form a:hover {
+	text-decoration: underline;
+}  
+</style>
+</head>
+<body>
+<div class="signup-form">
+    <form  method="POST" enctype="multipart/form-data" >
+		<h2>Fill Data</h2>
+		<p class="hint-text">Fill below form.</p>
+        <div class="form-group">
+			<div class="row">
+				<div class="col"><input type="text" class="form-control" name="fname" placeholder="Full Name" required="true"></div>
+				<div class="col"><input type="text" class="form-control" name="gender" placeholder="Gender" required="true"></div>
+			</div>        	
+        </div>
+        <div class="form-group">
+			<input type="text" class="form-control" name="contactno" placeholder="Enter Phone number" required="true" required="true" maxlength="10" pattern="[0-9]+">
+		</div>
+        <div class="form-group">
+		<input type="text" class="form-control" name="age" placeholder="Enter Age" required="true">
+
+        </div>
+		
+		<div class="form-group">
+            <textarea class="form-control" name="record" placeholder="Enter Medical Record" required="true"></textarea>
+        </div>  
+             <div class="form-group">
+        	<input type="file" class="form-control" name="profilepic"  required="true">
+        	<span style="color:red; font-size:12px;">Player (patient) Photo (Only jpg / jpeg/ png /gif format allowed)</span>
+        </div>      
+      
+		<div class="form-group">
+            <button type="submit" class="btn btn-success btn-lg btn-block" name="submit">Submit</button>
+        </div>
+    </form>
+	<div class="text-center">View Aready Inserted Data!!  <a href="home.php">View</a></div>
+</div>
+</body>
+</html>
